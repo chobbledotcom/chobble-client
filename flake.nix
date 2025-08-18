d11a70e36cd0609b33b9c9b2feea6afa5002dcd9
Stefan <git@stefn.co.uk>
2025-08-18 13:07:45 +0100

    Fix npm script aliases

--- flake.nix
+++ flake.nix
@@ -7,18 +7,22 @@
       devShells.x86_64-linux.default =
         let
           pkgs = import nixpkgs { system = "x86_64-linux"; };
+          npmScripts = pkgs.symlinkJoin {
+            name = "npm-scripts";
+            paths = map (cmd: pkgs.writeShellScriptBin cmd "npm run ${cmd}") [
+              "serve"
+              "build"
+              "prepare-dev"
+              "sync-files"
+              "watch"
+              "update-pages"
+              "clean"
+            ];
+          };
         in
         pkgs.mkShell {
-          buildInputs = [ pkgs.nodejs_23 ];
+          buildInputs = [ pkgs.nodejs_23 npmScripts ];
           shellHook = ''
-            alias serve="npm run serve"
-            alias build="npm run build"
-            alias prepare-dev="npm run prepare-dev"
-            alias sync-files="npm run sync-files"
-            alias watch="npm run watch"
-            alias update-pages="npm run update-pages"
-            alias clean="npm run clean"
-            
             cat <<EOF
 
             Available commands:
